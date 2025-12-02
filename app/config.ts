b3b6bf00ca59a34fb94621036c4afe573d2a1254
Mark-Qinfeng <dqdylan@gmail.com>
2025-12-02 11:19:12 +0800

    Update coins

--- app/config.ts
+++ app/config.ts
@@ -8,6 +8,12 @@ export const ASSET_CONFIG = [
     name: 'Ondo Finance' 
   },
   { 
+    id: 'tether-gold',      // CoinGecko ID (去 coingecko 网址上看 URL 里的名字)
+    defillamaId: 'tether-gold', // DefiLlama ID (通常和上面一样，偶尔不同)
+    symbol: 'XAUT', 
+    name: 'Tether Gold' 
+  },
+  { 
     id: 'maker', 
     defillamaId: 'makerdao', 
     symbol: 'MKR', 
@@ -30,6 +36,12 @@ export const ASSET_CONFIG = [
     defillamaId: 'goldfinch', 
     symbol: 'GFI', 
     name: 'Goldfinch' 
+  },
+  { 
+    id: 'realtoken-ecosystem-governance-gold',      // CoinGecko ID (去 coingecko 网址上看 URL 里的名字)
+    defillamaId: 'realt-tokens', // DefiLlama ID (通常和上面一样，偶尔不同)
+    symbol: 'REG', 
+    name: 'RealT' 
   },
   // 你可以在这里继续添加新的币种...
   // { id: 'truefi', defillamaId: 'truefi', symbol: 'TRU', name: 'TrueFi' },
